269d0cc6f5f3b39de7592bb38f6179984731ba2d
TejasvSingh <singhtejasv98@gmail.com>
2026-02-03 14:12:31 -0600

    Initial Commit

---
+++ Movie_CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_CollectionView
+//
+//  Created by Tejasv Singh on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
